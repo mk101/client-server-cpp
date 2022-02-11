f233efb7c39fcbd5915c512b47f21b30ec439e21
mk101 <pro100maks222@gmail.com>
2022-02-11 14:41:18 +0300

    Initial commit

---
+++ client/src/main.cpp
@@ -0,0 +1,8 @@
+#include <sdkddkver.h>
+#include <iostream>
+#include <boost/asio.hpp>
+
+int main() {
+  std::cout << "Hello, Client!" << std::endl;
+  return 0;
+}
---
+++ server/src/main.cpp
@@ -0,0 +1,8 @@
+#include <sdkddkver.h>
+#include <iostream>
+#include <boost/asio.hpp>
+
+int main() {
+  std::cout << "Hello, Server!" << std::endl;
+  return 0;
+}
