91e362f483d2dfbd03946bf47ac0904ab9c9099f
mk101 <pro100maks222@gmail.com>
2022-02-12 19:44:27 +0300

    Add UI to client

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -4,14 +4,33 @@
 #include "Client.h"
 
 int main() {
-  Client c;
-  c.start(tcp::endpoint(ip::address::from_string("127.0.0.1"), 8080));
-  c.sendMessage("get", 4);
-  std::cout << ResponseParser::parse(c.receivedMessage()).toString() << std::endl;
-  c.sendMessage("get", 4);
-  std::cout << ResponseParser::parse(c.receivedMessage()).toString() << std::endl;
-  c.sendMessage("get", 4);
-  std::cout << ResponseParser::parse(c.receivedMessage()).toString();
-  c.close();
+  Client client;
+  std::string answer;
+  try {
+    client.start(tcp::endpoint(ip::address::from_string("127.0.0.1"), 8080));
+  } catch (const std::exception &e) {
+    std::cerr << "Cannot connect to server";
+    exit(42);
+  }
+
+  while (true) {
+    std::cout << "Type 'get' to get free patient, or 'end' to close application" << std::endl;
+    std::cout << "> ";
+    std::cin >> answer;
+    if (answer != "get" && answer != "end") {
+      std::cout << "Unknown command" << std::endl;
+      continue;
+    }
+
+    if (answer == "get") {
+      client.sendMessage("get", 4);
+      auto patient = ResponseParser::parse(client.receivedMessage());
+      std::cout << "Free patient:" << std::endl << patient.toString()
+        << std::endl << "This patient will be blocked to another doctors, while you see him data" << std::endl;
+    } else if (answer == "end") {
+      client.close();
+      break;
+    }
+  }
   return 0;
 }
